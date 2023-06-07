9a5b1a850fbfde3c43fcc729e4be5513fa70f75d
Tomislav Pree <info@tomislavpree.com>
2023-06-07 14:49:41 +0200

    added workflows

---
+++ .github/workflows/production.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+REMOTE_PATH=/app/Algorillas
+HOST=3.66.147.23
+PORT=22
+
+function run_ssh() {
+  ssh -o StrictHostKeyChecking=no root@$HOST -p $PORT -i ~/.ssh/id_fit-in-finance -C "cd $REMOTE_PATH && $1"
+}
+
+function run_compose() {
+   run_ssh "docker-compose -f $REMOTE_PATH/production.yml $1"
+}
+
+run_ssh "mkdir -p $REMOTE_PATH/.envs/.production"
+run_ssh "echo '$APP' > $REMOTE_PATH/.envs/.production/.app"
+run_ssh "echo '$DB' > $REMOTE_PATH/.envs/.production/.db"
+
+run_ssh "git pull origin master"
+run_compose "build"
+sleep 10
+run_compose "up -d caddy"
