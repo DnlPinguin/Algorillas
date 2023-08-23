d8896994dbdeaf83c3f1a2190eafb194d752a345
Tomislav Pree <info@tomislavpree.com>
2023-08-23 20:18:15 +0200

    added footerr

--- src/components/Footer/FooterSection.vue
+++ src/components/Footer/FooterSection.vue
@@ -1,42 +1,131 @@
 <template>
-    <footer class="footer">
-        <div class="footer-container">
-            <div class="column ">
-                <h3>About us</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vitae.</p>
-            </div>
+<footer class="bg-white dark:bg-gray-800">
+  <div class="p-4 py-6 mx-auto max-w-screen-xl md:p-8 lg:p-10">
+    <p class="text-3xl font-bold leading-tight text-center dark:text-white">Feel connected anytime, anywhere.</p>
+      <div class="flex justify-center items-center my-6">
+          <a href="#" class="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800">Recieve News</a>
+          <a href="#" class="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Contact us</a>
+      </div>
 
-            <div class="column">
-                <h5>Was wir bieten</h5>
-                <ul>
-                    <li><a href="#">App Entwicklung</a></li>
-                    <li><a href="#">Web Enwticklung</a></li>
-                    <li><a href="#">App Design</a></li>
-                    <li><a href="#">Web Design</a></li>
-                    <li><a href="#">Entwickler mieten</a></li>
-                </ul>
-            </div>
-
-            <div class="column">
-                <h5>Informationen</h5>
-                <ul>
-                    <li><a href="#">Ablauf</a></li>
-                    <li><a href="#">Kosten</a></li>
-                    <li><a href="#">Contact Us</a></li>
-                    <li><a href="#">Contact Us</a></li>
-                    <li><a href="#">Contact Us</a></li>
-                </ul>
-            </div>
-            <div class="column">
-                <h5>Kontakt</h5>
-                <ul>
-                    <li><a href="tel:+4917641864586">+49 176 418 64 586</a></li>
-                    <li><a href="mailto:info@example.com">Email: info@example.com</a></li>
-                </ul>
-            </div>
-        </div>
-        <span class="copyright">© 2023 <b>ALGORILLAS</b>. All rights reserved.</span>
-    </footer>
+      <div class="grid grid-cols-2 gap-8 md:grid-cols-3 lg:grid-cols-5">
+          <div>
+              <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Company</h2>
+              <ul class="text-gray-500 dark:text-gray-400">
+                  <li class="mb-4">
+                      <a href="#" class=" hover:underline">About</a>
+                  </li>
+                  <li class="mb-4">
+                      <a href="#" class="hover:underline">Careers</a>
+                  </li>
+                  <li class="mb-4">
+                      <a href="#" class="hover:underline">Brand Center</a>
+                  </li>
+                  <li class="mb-4">
+                      <a href="#" class="hover:underline">Blog</a>
+                  </li>
+              </ul>
+          </div>
+          <div>
+              <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Help center</h2>
+              <ul class="text-gray-500 dark:text-gray-400">
+                  <li class="mb-4">
+                      <a href="#" class="hover:underline">Discord Server</a>
+                  </li>
+                  <li class="mb-4">
+                      <a href="#" class="hover:underline">Twitter</a>
+                  </li>
+                  <li class="mb-4">
+                      <a href="#" class="hover:underline">Facebook</a>
+                  </li>
+                  <li class="mb-4">
+                      <a href="#" class="hover:underline">Contact Us</a>
+                  </li>
+              </ul>
+          </div>
+          <div>
+              <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Legal</h2>
+              <ul class="text-gray-500 dark:text-gray-400">
+                  <li class="mb-4">
+                      <a href="#" class="hover:underline">Privacy Policy</a>
+                  </li>
+                  <li class="mb-4">
+                      <a href="#" class="hover:underline">Licensing</a>
+                  </li>
+                  <li class="mb-4">
+                      <a href="#" class="hover:underline">Terms</a>
+                  </li>
+              </ul>
+          </div>
+          <div>
+              <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Company</h2>
+              <ul class="text-gray-500 dark:text-gray-400">
+                  <li class="mb-4">
+                      <a href="#" class=" hover:underline">About</a>
+                  </li>
+                  <li class="mb-4">
+                      <a href="#" class="hover:underline">Careers</a>
+                  </li>
+                  <li class="mb-4">
+                      <a href="#" class="hover:underline">Brand Center</a>
+                  </li>
+                  <li class="mb-4">
+                      <a href="#" class="hover:underline">Blog</a>
+                  </li>
+              </ul>
+          </div>
+          <div>
+              <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Download</h2>
+              <ul class="text-gray-500 dark:text-gray-400">
+                  <li class="mb-4">
+                      <a href="#" class="hover:underline">iOS</a>
+                  </li>
+                  <li class="mb-4">
+                      <a href="#" class="hover:underline">Android</a>
+                  </li>
+                  <li class="mb-4">
+                      <a href="#" class="hover:underline">Windows</a>
+                  </li>
+                  <li class="mb-4">
+                      <a href="#" class="hover:underline">MacOS</a>
+                  </li>
+              </ul>
+          </div>
+      </div>
+      <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8">
+      <div class="text-center">
+          <a href="#" class="flex justify-center items-center mb-5 text-2xl font-semibold text-gray-900 dark:text-white">
+            <div class="animation-container algorillas-logo me-2">
+                            <img src="@/assets/logo-.svg">
+                        </div>
+              Algorillas    
+          </a>
+          <span class="block text-sm text-center text-gray-500 dark:text-gray-400">©<a href="#" class="hover:underline">Algorillas</a>. All Rights Reserved.
+          </span>
+          <ul class="flex justify-center mt-5 space-x-5">
+              <li>
+                  <a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white dark:text-gray-400">
+                      <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path fill-rule="evenodd" d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" clip-rule="evenodd" /></svg>
+                  </a>
+              </li>
+              <li>
+                  <a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white dark:text-gray-400">
+                      <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path fill-rule="evenodd" d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z" clip-rule="evenodd" /></svg>
+                  </a>
+              </li>
+              <li>
+                  <a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white dark:text-gray-400">
+                      <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" /></svg>
+                  </a>
+              </li>
+              <li>
+                  <a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white dark:text-gray-400">
+                      <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path fill-rule="evenodd" d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z" clip-rule="evenodd" /></svg>
+                  </a>
+              </li>
+          </ul>
+      </div>
+  </div>
+</footer>
 </template>
   
 <script>
@@ -46,54 +135,8 @@ export default {
 </script>
   
 <style scoped>
-.footer {
-    background: #f8f9fa;
-    color: black;
-    padding: 20px 0;
-    width: 100%;
-    text-align: center;
-}
-
-.footer-container {
-    display: grid;
-    grid-template-columns: 25% 25% 25% 25%;
-}
-
-.column {
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-}
-
-.column ul {
-    list-style: none;
-    padding: 0;
-}
-
-.column li {
-    margin-bottom: .5rem;
-}
-
-.column h5 {
-    margin-bottom: 1rem;
-}
-
-.column a {
-    color: black;
-    text-decoration: none;
-}
-
-.column p {
-    color: black;
-}
-
-.copyright {
-    margin-top: 1rem;
-    margin-bottom: 1rem;
-    display: block;
-    text-align: center;
-    width: 100%;
-
+.algorillas-logo {
+    width: 25px;
 }
 </style>
   
--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -4,17 +4,17 @@
 
         <div class="w-1/2 flex flex-col justify-center items-start ms-40">
             <div data-aos="fade-down" data-aos-duration="800">
-                <h1 :class="{'above-the-fold-claim-1': true, 'highlighted': headlines[0].highlighted  }">
+                <h1>
                     <span>
                         Research.
                     </span>
                 </h1>
-                <h1 :class="{'above-the-fold-claim-2': true, 'highlighted': headlines[1].highlighted  }">
+                <h1>
                     <span>
                         Collaborate.
                     </span>
                 </h1>
-                <h1 :class="{'above-the-fold-claim-3': true, 'highlighted': headlines[2].highlighted  }">
+                <h1>
                     <span>
                         Build.
                     </span>
@@ -63,27 +63,11 @@ export default {
                 "Profit.",
                 "Business Plans.",
             ],
-            partners: [
-            ],
-            services: [
-                "Web Development",
-                "Web Design",
-                "App Development",
-                "App Design",
-                "Marketing",
-                "Maintenance"
-            ],
-            headlines: [
-                { text: 'Überschrift 1', highlighted: true },
-                { text: 'Überschrift 2', highlighted: false },
-                { text: 'Überschrift 3', highlighted: false }
-            ]
         };
     },
 
     mounted() {
         this.writeFunnyIntroMessage()
-        setInterval(this.updateHighlightedHeadline, 2000);
     },
     methods: {
         writeFunnyIntroMessage() {
@@ -105,194 +89,11 @@ export default {
             setTimeout(this.eraseFunnyIntroMessage, 20);
         },
 
-        updateHighlightedHeadline() {
-            const highlightedIndex = this.headlines.findIndex(headline => headline.highlighted);
-            this.headlines[highlightedIndex].highlighted = false;
-            const nextIndex = (highlightedIndex + 1) % this.headlines.length;
-            this.headlines[nextIndex].highlighted = true;
-        }
     }
 }
 </script>
 <style scoped>
 
-.above-the-fold-claim-1 {
-  font-size: 69px;
-  font-weight: bold;
-  background: linear-gradient(
-    to right,
-    var(--myColor1-1) 20%,
-    var(--myColor2-1) 30%,
-    var(--myColor3-1) 70%,
-    var(--myColor4-1) 80%
-    );
-  -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent;
-  transition: --myColor1-1 1s, --myColor2-1 1s, --myColor3-1 1s, --myColor4-1 1s;
-
-}
-
-.above-the-fold-claim-2 {
-    margin-top: -20px;
-  font-size: 69px;
-  font-weight: bold;
-  background: linear-gradient(
-    to right,
-    var(--myColor1-2) 20%,
-    var(--myColor2-2) 30%,
-    var(--myColor3-2) 70%,
-    var(--myColor4-2) 80%
-    );
-  -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent;
-  transition: --myColor1-2 2s, --myColor2-2 2s, --myColor3-2 2s, --myColor4-2 2s;
-}
-
-.above-the-fold-claim-3 {
-    margin-top: -20px;
-  font-size: 69px;
-  font-weight: bold;
-  background: linear-gradient(
-    to right,
-    var(--myColor1-3) 20%,
-    var(--myColor2-3) 30%,
-    var(--myColor3-3) 70%,
-    var(--myColor4-3) 80%
-    );
-  -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent;
-  transition: --myColor1-3 2s, --myColor2-3 2s, --myColor3-3 2s, --myColor4-3 2s;
-}
-
-
-
-.highlighted.above-the-fold-claim-1 {
-    --myColor1-1: rgb(255, 106, 0);;
-    --myColor2-1: #E1AF2F;
-    --myColor3-1: green;
-    --myColor4-1: purple;
-    background-size: 500% auto;
-    animation: textShine 4s ease-in-out infinite alternate;
-  transition: --myColor1-1 2s, --myColor2-1 2s, --myColor3-1 2s, --myColor4-1 2s;
-
-}
-
-.highlighted.above-the-fold-claim-2 {
-    --myColor1-2: rgb(255, 106, 0);;
-    --myColor2-2: #E1AF2F;
-    --myColor3-2: green;
-    --myColor4-2: purple;
-    background-size: 500% auto;
-    animation: textShine 4s ease-in-out infinite alternate;
-  transition: --myColor1-2 2s, --myColor2-2 2s, --myColor3-2 2s, --myColor4-2 2s;
-
-}
-
-.highlighted.above-the-fold-claim-3 {
-    --myColor1-3: rgb(255, 106, 0);
-    --myColor2-3: #E1AF2F;
-    --myColor3-3: green;
-    --myColor4-3: purple;
-    background-size: 500% auto;
-    animation: textShine 4s ease-in-out infinite alternate;
-  transition: --myColor1-3 2s, --myColor2-3 2s, --myColor3-3 2s, --myColor4-3 2s;
-
-}
-
-
-@property --myColor1-1 {
-  syntax: '<color>';
-  initial-value: white;
-  inherits: false;
-}
-
-@property --myColor2-1 {
-  syntax: '<color>';
-  initial-value: white;
-  inherits: false;
-}
-
-@property --myColor3-1 {
-  syntax: '<color>';
-  initial-value: white;
-  inherits: false;
-}
-
-@property --myColor4-1 {
-  syntax: '<color>';
-  initial-value: white;
-  inherits: false;
-}
-
-
-
-
-
-
-
-@property --myColor1-2 {
-  syntax: '<color>';
-  initial-value: white;
-  inherits: false;
-}
-
-@property --myColor2-2 {
-  syntax: '<color>';
-  initial-value: white;
-  inherits: false;
-}
-
-@property --myColor3-2 {
-  syntax: '<color>';
-  initial-value: white;
-  inherits: false;
-}
-
-@property --myColor4-2 {
-  syntax: '<color>';
-  initial-value: white;
-  inherits: false;
-}
-
-
-
-
-
-
-@property --myColor1-3 {
-  syntax: '<color>';
-  initial-value: white;
-  inherits: false;
-}
-
-@property --myColor2-3 {
-  syntax: '<color>';
-  initial-value: white;
-  inherits: false;
-}
-
-@property --myColor3-3 {
-  syntax: '<color>';
-  initial-value: white;
-  inherits: false;
-}
-
-@property --myColor4-3 {
-  syntax: '<color>';
-  initial-value: white;
-  inherits: false;
-}
-
-
-
-@keyframes textShine {
-    0% {
-        background-position: 0% 50%;
-    }
-    100% {
-        background-position: 100% 50%;
-    }
-}
 
 
 .switching-phrase {
